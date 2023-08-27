971a356aa4abe0f9bf465ffecb75eed3c7f0a797
ilknurcoskun1 <ilknurgoner@gmail.com>
2023-08-27 07:18:11 +0100

    Initial commit

---
+++ CRMPlugins/ContactUpdatePlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+
+namespace CRMPlugins
+{
+    public class ContactUpdatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if (entity.LogicalName == "contact" && entity.Attributes.Contains("firstname") && context.Depth < 2)
+                {
+                    string updatedFirstName = entity["firstname"].ToString();
+                    entity["spousesname"] = updatedFirstName; // Update spousesname field
+                    service.Update(entity);
+                }
+            }
+        }
+    }
+}
